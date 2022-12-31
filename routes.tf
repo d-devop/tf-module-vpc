195e16613d095da84ba6bc7681facca5cd49b05e
Divya <divya5guntaka@gmail.com>
2022-12-31 14:29:39 +0530

    Update tf-modules/tf-module-vpc/

--- routes.tf
+++ routes.tf
@@ -3,3 +3,21 @@ resource "aws_route" "route_to_app_vpc_in_default_vpc" {
   destination_cidr_block    = var.vpc_cidr_block
   vpc_peering_connection_id = aws_vpc_peering_connection.peering.id
 }
+
+### Route tables
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}_public"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.env}_private"
+  }
+}
+
