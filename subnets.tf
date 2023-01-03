8ee2831b225f1a1692ac1fd16b33492948038b5a
Divya <divya5guntaka@gmail.com>
2023-01-03 12:08:25 +0530

    Update

--- subnets.tf
+++ subnets.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "public_subnets" {
   count      = length(var.public_subnet_cidr_block)
   vpc_id     = aws_vpc.main.id
   cidr_block = element(var.public_subnet_cidr_block, count.index)
+  availability_zone = element(var.subnet_azs, count.index)
 
   tags = {
     Name = "${var.env}-public-${count.index + 1}"
