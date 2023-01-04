fde0081fd202f2034ff4161e73413ecd273d270d
Divya <divya5guntaka@gmail.com>
2023-01-04 16:19:52 +0530

    Update

--- nacl.tf
+++ nacl.tf
@@ -1,6 +1,24 @@
 resource "aws_network_acl" "app" {
   vpc_id = aws_vpc.main.id
 
+  egress {
+    protocol   = "-1"
+    rule_no    = 1
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 32766
+    action     = "allow"
+    cidr_block = var.workstation_ip
+    from_port  = 22
+    to_port    = 22
+  }
+
   tags = {
     Name = "app"
   }
@@ -14,6 +32,15 @@ resource "aws_network_acl_association" "app" {
 
 resource "aws_network_acl" "db" {
   vpc_id = aws_vpc.main.id
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 1
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
 
   tags = {
     Name = "db"
