cac7c27f54b51a6eb392f3cdc2e72e3da3b463f2
Divya <divya5guntaka@gmail.com>
2023-01-05 19:39:55 +0530

    Update

--- vars.tf
+++ vars.tf
@@ -2,6 +2,7 @@ variable "env" {}
 variable "default_vpc_id" {}
 variable "default_vpc_cidr_block" {}
 variable "default_vpc_route_table" {}
+variable "workstation_ip" {}
 
 variable "vpc_cidr_block" {}
 variable "public_subnet_cidr_block" {}
