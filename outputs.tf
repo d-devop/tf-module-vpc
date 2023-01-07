728509db07a52f33f7e7cd7b893245c74ab65f70
Divya <divya5guntaka@gmail.com>
2023-01-07 09:44:27 +0530

    Update

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,19 @@ output "app_subnets_ids" {
 output "app_subnets_cidr" {
   value = aws_subnet.app_subnets.*.cidr_block
 }
+
+output "db_subnets_ids" {
+  value = aws_subnet.db_subnets.*.id
+}
+
+output "db_subnets_cidr" {
+  value = aws_subnet.db_subnets.*.cidr_block
+}
+
+output "public_subnets_ids" {
+  value = aws_subnet.public_subnets.*.id
+}
+
+output "public_subnets_cidr" {
+  value = aws_subnet.public_subnets.*.cidr_block
+}
